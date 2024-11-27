b59f3f526d72be06f3840f9a00ed238c4ce4429b
Jayc Santos <jayc@jaycsantos.com>
2024-11-27 16:23:54 +0800

    improve tags

--- src/app/components/tags.tsx
+++ src/app/components/tags.tsx
@@ -1,11 +1,21 @@
 export default function Tags({ tags }: { tags: string[] }) {
   return (
-    <ul className="flex flex-wrap gap-1 print:gap-0 print:block">
+    <ul className="inline-flex flex-wrap gap-1 print:gap-0 print:block">
       {tags.map((tag, tagIndex) => (
-        <li key={tagIndex} className="print:inline">
-          <code className="px-2 py-1 text-xs lowercase rounded-full print:px-0 print:py-0 print:bg-transparent bg-gray-400/25">
+        <li key={tagIndex} className="relative flex flex-row gap-0 text-xs print:inline ">
+          <div className="flex flex-none w-0 h-0 print:hidden
+            border-t-[.75rem] border-t-transparent
+            border-r-[.25rem] border-gray-400/20
+            border-b-[.75rem] border-b-transparent">
+          </div>
+          <code className="flex-1 px-1 py-1 lowercase print:px-0 print:py-0 print:bg-transparent bg-gray-400/20">
             {tag}<span className="hidden print:inline">,&nbsp;</span>
           </code>
+          <div className="absolute -right-[0.25rem] w-0 h-0 print:hidden 
+            border-t-[.75rem] border-t-gray-400/20
+            border-r-[.25rem] border-transparent
+            border-b-[.75rem] border-b-gray-400/20">
+          </div>
         </li>
       ))}
     </ul>
